25b746ce85f035b0300f99f332696a965b5e2a7e
elizabeth-agada <elizadave6@gmail.com>
2023-08-16 15:48:27 +0100

    created the landing page for all events

--- src/page/LandingPage.jsx
+++ src/page/LandingPage.jsx
@@ -1,205 +1,159 @@
-import { useLayoutEffect, useRef } from 'react'
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-import Footer from "../components/Footer"
-import NavBar from "../components/Navbar"
-
-
-
-gsap.registerPlugin(ScrollTrigger)
-
-
-
-
-
-
-
-
+import React from 'react';
+import NavBar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 
 const LandingPage = () => {
-
-  const containerRef = useRef(null)
-  const contentRef = useRef(null)
-
-
-
-
-  useLayoutEffect(() => {
-    if (window.innerWidth > 768) {
-      const timelineAnimation = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          pin: true,
-          scrub: 1,
-          end: `+=6000`
-        }
-      })
-
-      timelineAnimation.to(contentRef.current, { y: "-75%" })
-
-      return () => timelineAnimation.revert()
-    }
-  }, [])
-
-
-
-
-
-
-
   return (
     <>
       <NavBar />
       
       <div className='overflow-hidden'>
-        <section ref={containerRef} className='w-screen h-screen sm:overscroll-y-auto overflow-hidden text-white flex flex-col justify-center relative' style={{ background:"url('/img/heroImg.png')", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundSize:"cover" }}>
-      
-          <section ref={contentRef} className="overflow-auto w-full bg-black/80 sm:absolute sm:top-0">
-            <section className="min-h-screen">
-              <section className="h-[85vh] parent-size flex items-center">
-                <div className="grid grid-cols-12 gap-y-3 subHeading-text digitalFont font-light w-full">
-                  <div className="col-span-12 sm:col-span-9 sm:row-span-3 order-2 sm:order-1">
-                    <h1 className="sm:text-9xl">TECH</h1>
-                    <h1 className="sm:text-9xl">eXperience</h1>
-                    <h1 className="">Taraba 2023</h1>
-                  </div>
-                  
-                  <div className="col-span-12 sm:col-span-3 sm:row-span-2 order-1 sm:order-2 sm:flex sm:items-end">
-                    <p className="text-xxs leading-normal max-w-xxs sm:max-w-fit ml-auto sm:ml-0">
-                      {/* Ignite Innovation! Unleash the future at our immersive tech event. Connect, explore, and shape tomorrow with industry leaders */}
-                      Activate Tomorrow! Join us for an interactive technology event where you can meet, discover, and build the future with industry experts.
-                    </p>
-                  </div>
-                  
-                  <div className="col-span-12 sm:col-span-3 order-3 sm:flex sm:items-end">
-                    <a href='/register' className="bg-[#003380] py-3 px-6 rounded-md text-lg text-white font-medium">Reserve Your Spot</a>
-                  </div>
-                </div>
-              </section>
-
-
-              <div className="h-[10vh] digitalFont">
-                <h6 className="space-x-1">
-                  <i className="bi bi-geo-alt"></i>
-                  <span className="font-light">DSK Foundation, beside Fidelity Bank, Jalingo, Taraba State</span>
-                </h6>
-                
-                <a href='/register' className="bg-[#000] w-48 py-3 rounded-r-md text-base text-white uppercase flex items-center justify-center gap-x-2">
-                  <span className="">Jalingo</span>
-                  <span className="text-2xl text-[#3fff00] font-normal">29</span>
-                  <span className="">July</span>
-                </a>
-                {/* <a href='/register' className="bg-[#3FFF00] px-4 py-4 rounded-r-md text-base text-white font-medium">Reserve Your Spot</a> */}
-              </div>
-            </section>
-
-
-            <section className="min-h-screen parent-size flex items-center">
-              <p className="w-full max-w-sm ml-auto uppercase font-normal text-sm">
-                Keynote Speeches, Interactive Sessions, Emerging Technologies Showcase, Networking Opportunities, Tech Talks, Competitions, Startup Showcase, Tech Ethics and Impact Discussions, Tech Expo, Industry Insights
-              </p>
-            </section>
-
-
-            <section className="min-h-screen parent-size space-y-8">
-              <h4 className="digitalFont subHeading-text">INSIGHT</h4>
-
-              <div className="w-full max-w-3xl mx-auto border-8 border-white rounded-lg h-[30rem]" style={{ background: "url(/img/insight.gif)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
-            </section>
-
-
-            <section className="min-h-screen flex items-center py-24">
-              <div className="parent-size space-y-14">
-                <h4 className="digitalFont subHeading-text">3 SPEAKERS</h4>
-
-                <div className="grid grid-cols-12 gap-8">
-                  <aside className="col-span-12 sm:col-span-4 bg-white rounded-md overflow-hidden">
-                    <section className="h-96 overflow-hidden">
-                      <div className="w-full h-96" style={{ background: "url('/img/Gideon Nonso.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}></div>
-                    </section>
-
-                    <div className="p-4 text-black">
-                      <h1 className="text-2xl font-semibold">Gideon Nonso</h1>
-                      <h6 className="text-base leading-none">Taraba State Commissioner of Digital Economy and Innovation</h6>
-                    </div>
-                    {/* <img src="/img/joinUs.jpg" alt="" className="w-full max-w-xl" /> */}
-                  </aside>
-                  
-                  <aside className="col-span-12 sm:col-span-4 bg-white rounded-md overflow-hidden">
-                    <section className="h-96 overflow-hidden">
-                      <div className="w-full h-96" style={{ background: "url(https://i.quotev.com/2kr3itdgaaaa.jpg)", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}></div>
-                    </section>
-
-                    <div className="p-4 text-black">
-                      <h1 className="text-2xl font-semibold">Penelope</h1>
-                      <h6 className="text-base leading-none">AI voiceover character from Revoicer</h6>
-                    </div>
-                    {/* <img src="https://www.baps.org.uk/content/uploads/2020/05/mystery-person.jpg" alt="" className="w-full max-w-lg" /> */}
-                  </aside>
-                  
-                  <aside className="col-span-12 sm:col-span-4 bg-white rounded-md overflow-hidden">
-                    <section className="h-96 overflow-hidden">
-                      <div className="w-full h-96" style={{ background: "url('/img/team/jon.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}></div>
-                    </section>
-
-                    <div className="p-4 text-black">
-                      <h1 className="text-2xl font-semibold">John Okeke</h1>
-                      <h6 className="text-base leading-none">Founder, Sandlip Digital Consulting</h6>
-                    </div>
-                    {/* <img src="/img/joinUs.jpg" alt="" className="w-full max-w-xl" /> */}
-                  </aside>
+        <section className=" w-full flex flex-col justify-center" style={{ background:"url('/img/heroImg.png')", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundSize:"cover" }}>
+          <section className="bg-black/80">
+            <section className="flex items-center p-5 py-20">
+              <div className="sm:col-span-2 text-white subHeading-text">
+                <h1 className="sm:text-9xl text-3xl">EVENT</h1>
+                <div className="flex justify-start items-center">
+                  <i class="bi bi-arrow-right sm:text-9xl hidden sm:block"></i>
+                 <h1 className="sm:text-9xl text-3xl">LISTING</h1>
+                 <i class="bi bi-arrow-down sm:text-9xl sm:hidden text-3xl"></i>
                 </div>
               </div>
             </section>
+            <div className="sm:grid grid-cols-12 text-white p-5 gap-10 space-y-4 sm:space-y-0">
+              <div className="col-span-4">
+                <div className="space-y-4">
+                  <div className="">
+                    <p className="">29.04.23</p>
+                  </div>
+                  <div className="sm:text-3xl">
+                    <h1 className="">TECH EXPOSE</h1>
+                    <div className="flex justify-start items-center">
+                      <i class="bi bi-arrow-right text-3xl"></i>
+                      <h1 className="">TARABA</h1>
+                    </div>
+                  </div>
+                  <div className="text-sm w-full sm:w-2/3">
+                  Providing a platform for attendees to learn about emerging trends and strategies for staying competitive in the tech industry.
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-4">
+                <div className="space-y-4">
+                  <div className="">
+                    <p className="">29.07.23</p>
+                  </div>
+                  <div className="sm:text-3xl">
+                    <h1 className="">TECH eXperience</h1>
+                    <div className="flex justify-start items-center">
+                      <i class="bi bi-arrow-right text-3xl"></i>
+                      <h1 className="">TARABA</h1>
+                    </div>
+                  </div>
+                  <div className="text-sm w-full sm:w-2/3">
+                  Showcasing the latest advancements in technology, including Artificial Intelligence, digitalization, and the opportunity to network.
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-4">
+                <div className="space-y-4">
+                  <div className="">
+                    <p className="">09-11.11.23</p>
+                  </div>
+                  <div className="sm:text-3xl">
+                    <h1 className="">CR8 TARABA</h1>
+                    <div className="flex justify-start items-center">
+                      <i class="bi bi-arrow-right text-3xl"></i>
+                      <h1 className="">HACKATHON</h1>
+                    </div>
+                  </div>
+                  <div className="text-sm w-full sm:w-2/3">
+                  Leveraging problem-based learning practices to solve problems creatively and collaboratively.
+                  </div>
+                </div>
+              </div>
+            </div>
           </section>
         </section>
 
-      
+        <section className="sm:space-y-4">
+          <div className="flex justify-start items-center p-7">
+            <i class="bi bi-arrow-right sm:text-9xl text-3xl"></i>
+            <h2 className="sm:text-9xl text-3xl">PAST</h2>
+          </div>
 
-        <section className="py-24 relative parent-size grid grid-cols-12 gap-y-10 sm:gap-y-20">
-          <aside className="col-span-12 sm:col-span-7 relative">
-            <img src="/img/joinUs.jpg" alt="" className="rounded-md w-full max-w-xl" />
-
-            <div className="absolute top-0 w-full max-w-xl h-full bg-black/25 rounded-md"></div>
-
-            <div className="absolute -top-6 left-4 shadow-top sm:w-[12rem] rounded-md bg-white px-5 pb-3 font-bold">
-              <h6 className="gradient-text text-7xl">1</h6>
-              <h6 className="text-base text-black">Impactful Day</h6> 
+          <div className="sm:grid grid-cols-12 gap-10 p-7 space-y-10 sm:space-y-0">
+            <div className="col-span-3" >
+              <a href="/upcoming" className="">
+                <img src="/img/joinUs.jpg" alt="" className="hover:opacity-80" />
+                <div className="space-y-2">
+                  <div className="sm:text-3xl">
+                    <h1 className="">TECH EXPOSE</h1>
+                    <div className="flex justify-start items-center">
+                      <i class="bi bi-arrow-right text-3xl"></i>
+                      <h1 className="">TARABA</h1>
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                  A dive into the latest trends and advancements, showcasing the transformative power of technology. Connect with like-minded individuals for growth and collaboration.
+                  </div>
+                </div>
+              </a>
             </div>
-          </aside>
-
-          <aside className="col-span-12 sm:col-span-5 p-2">
-            <h2 className="subHeading-text">Join Tech eXperience Taraba</h2>
-            <h2 className="subHeading-text">2023</h2>
-          </aside>
-          
-
-          <aside className="col-span-12 sm:col-span-7 flex flex-col gap-y-14 sm:gap-y-0 order-4 sm:order-3 mt-14 sm:mt-0">
-            <article className="sm:flex-1 w-full max-w-xl">
-              Connect with like-minded individuals, professionals, and community leaders. This event offers a platform for meaningful conversations and collaborations, fostering a stronger and more vibrant community. Don't miss out on this incredible event! Mark your calendars, spread the word, and get ready to join us for a day of connection, celebration, and community building. We can't wait to see you there!
-            </article>
-
-            <div className="">
-              <a href='/register' className="bg-[#003380] py-4 px-6 sm:py-3.5 rounded-md text-lg text-white font-medium">Reserve Your Spot</a>
+           
+            <div className="col-span-3">
+              <a href="/upcoming" className="">
+                <img src="/img/TechX-1.JPG" alt="" className="hover:opacity-80" />
+                <div className="space-y-2">
+                  <div className="sm:text-3xl">
+                    <h1 className="">TECH eXperience</h1>
+                    <div className="flex justify-start items-center">
+                      <i class="bi bi-arrow-right text-3xl"></i>
+                      <h1 className="">TARABA</h1>
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                  A gathering of industry leaders, experts, and enthusiasts to explore the latest advancements and opportunities in the realm of technology.
+                  </div>
+                </div>
+              </a>
             </div>
-          </aside>
-
-          <aside className="col-span-12 sm:col-span-5 relative max-w-sm mx-auto order-3 sm:order-4">
-            <img src="/img/joinUs.jpg" alt="" className="sm:rounded-md" />
-
-            <div className="absolute -bottom-6 right-4 shadow-2xl rounded-md bg-white pb-3 px-5 font-bold">
-              <h6 className="gradient-text text-7xl">3</h6>
-              <h6 className="text-base text-black mt-2">Experienced Speakers</h6> 
+            <div className="col-span-3">
+              <a href="/upcoming" className="">
+                <img src="/img/joinUs.jpg" alt="" className="hover:opacity-80" />
+                <div className="space-y-2">
+                  <div className="sm:text-3xl">
+                    <h1 className="">CR8 Taraba</h1>
+                    <div className="flex justify-start items-center">
+                      <i class="bi bi-arrow-right text-3xl"></i>
+                      <h1 className="">Hackathon</h1>
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                  An event that brings together people from different backgrounds and skill sets to work collaboratively on solving problems and developing new ideas.
+                  </div>
+                </div>
+              </a>
             </div>
-          </aside>
+            <div className="col-span-3 flex flex-col items-center">
+              <div className="">
+                <img src="/img/joinUs.jpg" alt="" className="" />
+              </div>
+              <div className="">
+                <svg xmlns="http://www.w3.org/2000/svg" class="animate-bounce h-full w-full" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 3a1 1 0 0 1 .707.293l4 4a1 1 0 0 1-1.414 1.414L11 6.414V16a1 1 0 1 1-2 0V6.414L5.707 8.707A1 1 0 0 1 4.293 7.293l4-4A1 1 0 0 1 10 3z" clip-rule="evenodd" />
+                </svg>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
-
       <Footer />
     </>
   )
 }
 
 export default LandingPage
+        
+    
--- src/page/UpcomingEvent.jsx
+++ src/page/UpcomingEvent.jsx
@@ -1,42 +1,159 @@
-import React from 'react';
-import NavBar from '../components/Navbar';
-import Footer from '../components/Footer';
+import { useLayoutEffect, useRef } from 'react'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import Footer from "../components/Footer"
+import NavBar from "../components/Navbar"
+
+
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+
+
+
+
+
+
 
 
 const UpcomingEvent = () => {
+
+  const containerRef = useRef(null)
+  const contentRef = useRef(null)
+
+
+
+
+  useLayoutEffect(() => {
+    if (window.innerWidth > 768) {
+      const timelineAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          pin: true,
+          scrub: 1,
+          end: `+=6000`
+        }
+      })
+
+      timelineAnimation.to(contentRef.current, { y: "-75%" })
+
+      return () => timelineAnimation.revert()
+    }
+  }, [])
+
+
+
+
+
+
+
   return (
     <>
       <NavBar />
       
       <div className='overflow-hidden'>
-        <section className='w-screen h-screen relative' style={{ background:"url('/img/heroImg.png')", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundSize:"cover" }}>
-          <div className='absolute top-0 w-full h-screen bg-black/70 text-white flex items-center'>
-
-            <section className="parent-size space-y-20 sm:space-y-16">
-              <div className="space-y-16">
-                <div className="subHeading-text">
-                  <h1 className="sm:text-8xl">TECH eXperience</h1>
-                  <h1 className="">Taraba</h1>
+        <section ref={containerRef} className='w-screen h-screen sm:overscroll-y-auto overflow-hidden text-white flex flex-col justify-center relative' style={{ background:"url('/img/heroImg.png')", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundSize:"cover" }}>
+      
+          <section ref={contentRef} className="overflow-auto w-full bg-black/80 sm:absolute sm:top-0">
+            <section className="min-h-screen">
+              <section className="h-[85vh] parent-size flex items-center">
+                <div className="grid grid-cols-12 gap-y-3 subHeading-text digitalFont font-light w-full">
+                  <div className="col-span-12 sm:col-span-9 sm:row-span-3 order-2 sm:order-1">
+                    <h1 className="sm:text-9xl">TECH</h1>
+                    <h1 className="sm:text-9xl">eXperience</h1>
+                    <h1 className="">Taraba 2023</h1>
+                  </div>
+                  
+                  <div className="col-span-12 sm:col-span-3 sm:row-span-2 order-1 sm:order-2 sm:flex sm:items-end">
+                    <p className="text-xxs leading-normal max-w-xxs sm:max-w-fit ml-auto sm:ml-0">
+                      {/* Ignite Innovation! Unleash the future at our immersive tech event. Connect, explore, and shape tomorrow with industry leaders */}
+                      Activate Tomorrow! Join us for an interactive technology event where you can meet, discover, and build the future with industry experts.
+                    </p>
+                  </div>
+                  
+                  <div className="col-span-12 sm:col-span-3 order-3 sm:flex sm:items-end">
+                    <a href='/register' className="bg-[#003380] py-3 px-6 rounded-md text-lg text-white font-medium">Reserve Your Spot</a>
+                  </div>
                 </div>
+              </section>
 
-                <div className="">
-                  <a href='/register' className="bg-[#FE7A67] py-3 px-4 sm:py-3.5 rounded-md text-base text-white font-medium">Reserve Your Spot</a>
-                </div>
-              </div>
 
-              <div className="flex items-center sm:gap-x-10 gap-x-5">
-                <section className="flex items-center gap-x-2 text-base">
-                  <div className="border h-7 w-7 flex items-center justify-center rounded-full"> <i className="text-sm bi bi-calendar3"></i> </div>
-                  <h2 className="font-medium">July 29, 2023</h2>
-                </section>
-
-                <section className="flex items-center gap-x-2 text-base">
-                  <div className="border h-7 w-7 flex items-center justify-center rounded-full"> <i className="text-sm bi bi-geo-alt-fill"></i> </div>
-                  <h2 className="font-medium">Jalingo, Taraba</h2>
-                </section>
+              <div className="h-[10vh] digitalFont">
+                <h6 className="space-x-1">
+                  <i className="bi bi-geo-alt"></i>
+                  <span className="font-light">DSK Foundation, beside Fidelity Bank, Jalingo, Taraba State</span>
+                </h6>
+                
+                <a href='/register' className="bg-[#000] w-48 py-3 rounded-r-md text-base text-white uppercase flex items-center justify-center gap-x-2">
+                  <span className="">Jalingo</span>
+                  <span className="text-2xl text-[#3fff00] font-normal">29</span>
+                  <span className="">July</span>
+                </a>
+                {/* <a href='/register' className="bg-[#3FFF00] px-4 py-4 rounded-r-md text-base text-white font-medium">Reserve Your Spot</a> */}
               </div>
             </section>
-          </div>
+
+
+            <section className="min-h-screen parent-size flex items-center">
+              <p className="w-full max-w-sm ml-auto uppercase font-normal text-sm">
+                Keynote Speeches, Interactive Sessions, Emerging Technologies Showcase, Networking Opportunities, Tech Talks, Competitions, Startup Showcase, Tech Ethics and Impact Discussions, Tech Expo, Industry Insights
+              </p>
+            </section>
+
+
+            <section className="min-h-screen parent-size space-y-8">
+              <h4 className="digitalFont subHeading-text">INSIGHT</h4>
+
+              <div className="w-full max-w-3xl mx-auto border-8 border-white rounded-lg h-[30rem]" style={{ background: "url(/img/insight.gif)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}></div>
+            </section>
+
+
+            <section className="min-h-screen flex items-center py-24">
+              <div className="parent-size space-y-14">
+                <h4 className="digitalFont subHeading-text">3 SPEAKERS</h4>
+
+                <div className="grid grid-cols-12 gap-8">
+                  <aside className="col-span-12 sm:col-span-4 bg-white rounded-md overflow-hidden">
+                    <section className="h-96 overflow-hidden">
+                      <div className="w-full h-96" style={{ background: "url('/img/Gideon Nonso.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}></div>
+                    </section>
+
+                    <div className="p-4 text-black">
+                      <h1 className="text-2xl font-semibold">Gideon Nonso</h1>
+                      <h6 className="text-base leading-none">Taraba State Commissioner of Digital Economy and Innovation</h6>
+                    </div>
+                    {/* <img src="/img/joinUs.jpg" alt="" className="w-full max-w-xl" /> */}
+                  </aside>
+                  
+                  <aside className="col-span-12 sm:col-span-4 bg-white rounded-md overflow-hidden">
+                    <section className="h-96 overflow-hidden">
+                      <div className="w-full h-96" style={{ background: "url(https://i.quotev.com/2kr3itdgaaaa.jpg)", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}></div>
+                    </section>
+
+                    <div className="p-4 text-black">
+                      <h1 className="text-2xl font-semibold">Penelope</h1>
+                      <h6 className="text-base leading-none">AI voiceover character from Revoicer</h6>
+                    </div>
+                    {/* <img src="https://www.baps.org.uk/content/uploads/2020/05/mystery-person.jpg" alt="" className="w-full max-w-lg" /> */}
+                  </aside>
+                  
+                  <aside className="col-span-12 sm:col-span-4 bg-white rounded-md overflow-hidden">
+                    <section className="h-96 overflow-hidden">
+                      <div className="w-full h-96" style={{ background: "url('/img/team/jon.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}></div>
+                    </section>
+
+                    <div className="p-4 text-black">
+                      <h1 className="text-2xl font-semibold">John Okeke</h1>
+                      <h6 className="text-base leading-none">Founder, Sandlip Digital Consulting</h6>
+                    </div>
+                    {/* <img src="/img/joinUs.jpg" alt="" className="w-full max-w-xl" /> */}
+                  </aside>
+                </div>
+              </div>
+            </section>
+          </section>
         </section>
 
       
@@ -49,7 +166,7 @@ const UpcomingEvent = () => {
 
             <div className="absolute -top-6 left-4 shadow-top sm:w-[12rem] rounded-md bg-white px-5 pb-3 font-bold">
               <h6 className="gradient-text text-7xl">1</h6>
-              <h6 className="text-base">Impactful Day</h6> 
+              <h6 className="text-base text-black">Impactful Day</h6> 
             </div>
           </aside>
 
@@ -74,10 +191,15 @@ const UpcomingEvent = () => {
 
             <div className="absolute -bottom-6 right-4 shadow-2xl rounded-md bg-white pb-3 px-5 font-bold">
               <h6 className="gradient-text text-7xl">3</h6>
-              <h6 className="text-base mt-2">Experienced Speakers</h6> 
+              <h6 className="text-base text-black mt-2">Experienced Speakers</h6> 
             </div>
           </aside>
         </section>
+      </div>
+      <div className="flex p-2">
+        <button className=" max-w-xs  ml-auto text-lg bg-[#003380] py-4 px-6 sm:py-3.5 rounded-md text-white font-medium">
+          Upcoming Event
+        </button>
       </div>
 
       <Footer />
