fad16c0ad8ab4984436ad6d0556255d82a19affd
elizabeth-agada <elizadave6@gmail.com>
2023-09-04 20:58:00 +0100

    created CR8 Hackathon page

--- src/page/RegisterPage.jsx
+++ src/page/RegisterPage.jsx
@@ -26,18 +26,24 @@ const RegisterPage = ({ registrantInfo, setRegistrantInfo, postNewEntry, formIsS
     <div className="sm:min-h-screen space-y-6 py-6">
       <section className="min-h-screen flex items-center justify-center px-4">
         <div className="w-full max-w-lg mx-auto border border-gray-300 rounded-lg shadow-lg py-10 sm:px-8 px-4 font-medium space-y-6">
-          <div className="w-fit mx-auto">
+          {/*<div className="w-fit mx-auto">
             <a href="/" className=""> <img src="/img/logo/logo-black.png" alt="SANDLIP" className="h-6" /> </a>
-          </div>
+          </div>*/}
 
-          <h6 className="text-xl">
+          {/*<h6 className="text-xl">
             Thank you for you interest to be a participants of Tech eXperience. The event has ended but we look forward to having you with us in the next one.
+          </h6>*/}
+
+           <h6 className="text-xl">
+            Thank you for you interest to be a participants of CR8 HACKATHON TARABA 2023.
           </h6>
 
-          <h6 className="text-xl">
+          {/*<h6 className="text-xl">
             Want to be a part of the community to get first hand information about our events? <a href="https://wa.me/+2349161688902?text=Hi, My name is" className="underline text-[#003380]">Click here to tell us your full name</a>, you will be added to the community ASAP.
-          </h6>
+          </h6>*/}
         </div>
+
+        
         {/* <div className="w-full max-w-lg mx-auto border border-gray-300 rounded-lg shadow-lg py-10 sm:px-8 px-4 font-medium space-y-6">
           <div className="w-fit mx-auto">
             <a href="/" className=""> <img src="/img/logo/logo-black.png" alt="SANDLIP" className="h-6" /> </a>
