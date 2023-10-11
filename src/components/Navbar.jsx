3716bbc7e941efc7dfb35e6594ecae6b3559c87b
jorniks <dictforum@gmail.com>
2023-10-11 17:00:19 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const NavBar = () => {
         </a>
         
 
-        <a href="/sponsor" className={`${isScrolled ? "bg-black text-white" : "bg-white text-black" } px-2.5 sm:px-3 py-2.5 sm:py-3 rounded-md text-sm font-medium`}>Become a Partner</a>
+        {/* <a href="/sponsor" className={`${isScrolled ? "bg-black text-white" : "bg-white text-black" } px-2.5 sm:px-3 py-2.5 sm:py-3 rounded-md text-sm font-medium`}>Become a Partner</a> */}
       </div>
     </div>
   )
