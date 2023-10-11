27d44c0c99db059a085f8fc0965e8546dc23572b
jorniks <dictforum@gmail.com>
2023-10-11 15:41:49 +0100

    update to splash screen file

--- src/page/SplashPage.jsx
+++ src/page/SplashPage.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 const SplashPage = () => {
   return (
-    <div>
-      SplashPage
+    <div className="min-h-screen w-full flex items-center justify-center px-8">
+      <img src="/img/logo/logo-black.png" alt="" className="w-auto" />
     </div>
   )
 }
