b505939144e7e65cab4273bc8b677b2d9111c931
jorniks <dictforum@gmail.com>
2026-05-08 17:01:19 +0100

    feat: added dummy registration link

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { PiArrowUpRight } from 'react-icons/pi'
 
 const RegisterButton = ({ buttonClass, label, arrowClass = "py-2.5 px-3.5" }: { buttonClass: string; label: string; arrowClass?: string }) => {
-  const registrationLink = ""
+  const registrationLink = "https://sandlip.com"
 
   return (
     <Link href={registrationLink} className="flex items-center group cursor-not-allowed"
